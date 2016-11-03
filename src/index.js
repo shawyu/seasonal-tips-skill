b8d6641db3358dac4636c6150dcd3ca6275f658f
Shaw Yu <shaw.yu@gmail.com>
2016-11-03 14:52:41 -0400

    bare bones tips skill

--- src/index.js
+++ src/index.js
@@ -1,36 +1,91 @@
 'use strict'
 
-var alexa = require('alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.5cc7c96d-b14a-40bf-9a69-e49853f133e8',
+    AlexaSkill = require('./AlexaSkill'),
+    moment = require('moment-timezone'),
+    tipsService = require('../lib/seasonal-tips'),
+    SeasonalTipsSkill = function() {
+        AlexaSkill.call(this, APP_ID);
+    };
 
-var handlers = {
-    "LaunchRequest": function() {
-        var speechText = 'Welcome to this useful skill!',
-            repromptText = 'For instructions on what you can say, please say help me.';
+SeasonalTipsSkill.prototype = Object.create(AlexaSkill.prototype);
+SeasonalTipsSkill.prototype.constructor = SeasonalTipsSkill;
 
-        this.emit(':ask', speechText, repromptText);
-    },
-    "TipIntent": function() {
-        var speechText = 'Don\'t eat yellow snow!';
+SeasonalTipsSkill.prototype.eventHandlers.onSessionStarted = function(request, session) {
+    console.log("SeasonalTipsSkill onSessionStarted requestId: "
+        + request.requestId
+        + ", sessionId: " + session.sessionId);
 
-        this.emit(':tell', speechText);
-    },
-    "AMAZON.HelpIntent": function() {
-        var speechText = 'How can I help?'
+}
 
-        this.emit(':ask', speechText, speechText);
+SeasonalTipsSkill.prototype.eventHandlers.onLaunch = function(request, session, response) {
+    console.log("SeasonalTipsSkill onLaunch requestId: "
+        + request.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // get welcome response(response)
+    var speechText = 'Welcome to this useful skill!',
+        repromptText = 'For instructions on what you can say, please say help me.';
+
+    response.ask(
+        getSSMLSpeech(speechText),
+        getSSMLSpeech(repromptText)
+    );
+}
+
+SeasonalTipsSkill.prototype.eventHandlers.onSessionEnded = function(request, session) {
+    console.log("SeasonalTipsSkill onSessionEnded requestId: "
+        + request.requestId
+        + ", sessionId: " + session.sessionId);
+}
+
+SeasonalTipsSkill.prototype.intentHandlers = {
+    "TipIntent": function(intent, session, response) {
+        var season = getSeasonForMoment(moment()),
+            speechText = tipsService.getTipTextForSeason(season);
+
+        response.tell(getSSMLSpeech(speechText));
     },
-    "AMAZON.StopIntent": function () {
-        this.emit(':tell', 'Goodbye');
+    "AMAZON.HelpIntent": function(intent, session, response) {
+        var speechText = 'How can I help?';
+
+        response.ask(
+            getSSMLSpeech(speechText),
+            getSSMLSpeech(speechText)
+        );
     },
-    "AMAZON.CancelIntent": function () {
-        this.emit(':tell', 'Goodbye');
+    "AMAZON.StopIntent": function(intent, session, response) {
+        response.tell(getSSMLSpeech('Goodbye'));
+    },
+    "AMAZON.CancelIntent": function(intent, session, response) {
+        response.tell(getSSMLSpeech('Goodbye'));
     }
 };
 
-exports.handler = function(event, context) {
-    var alexaInstance = alexa.handler(event, context);
+function getSeasonForMoment(moment) {
+    if (!moment) {
+        return undefined;
+    }
+    if (moment.month >= 3 && moment.month <= 5) {
+        return 'SPRING';
+    }
+    if (moment.month >= 6 && moment.month <= 8) {
+        return 'SUMMER';
+    }
+    if (moment.month >= 9 && moment.month <= 11) {
+        return 'FALL';
+    }
+    return 'WINTER';
+}
 
-    //alexaInstance.appId = 'amzn1.echo-sdk-ams.app.APP_ID';
-    alexaInstance.registerHandlers(handlers);
-    alexaInstance.execute();
+function getSSMLSpeech(text) {
+    return {
+        speech: text,
+        type: AlexaSkill.speechOutputType.SSML
+    }
+}
+
+exports.handler = function(event, context) {
+    var skill = new SeasonalTipsSkill();
+    skill.execute(event, context);
 };
